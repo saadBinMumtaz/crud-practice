97d5965391c86e33fd8bd66f5fc0ffba8d67d259
saadbm369 <saadbm369@gmail.com>
2026-08-11 23:45:08 +0500

    added user model

---
+++ models/user.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/userdb');
+
+const userSchema = mongoose.Schema({
+  name: String,
+  email: String,
+  imageurl: String
+});
+
+module.exports = mongoose.model('User', userSchema);
